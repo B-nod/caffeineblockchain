adc36519a618e5c87b80015fc8ba87dfdaf98818
B-nod <binodtamang245@gmail.com>
2023-09-25 21:19:42 +0545

    a

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,2 @@
-echo "BUILD START"
-
-python3.10.1 -m pip install -r requirements.txt
-python3.10.1 manage.py collectstatic  --noinput  --clear
-
-echo "BUILD END"
+pip install -r requirements.txt 
+python3.9 manage.py collectstatic
